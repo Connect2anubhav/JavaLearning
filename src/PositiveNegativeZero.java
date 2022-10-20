88b6155812232e9bb97e83b1721ef29ebf918ff6
Anubhav <Connect2anusinha@gmai.com>
2022-10-20 08:35:56 +0530

    adding one file PostiveNegativeZero

--- src/PositiveNegativeZero.java
+++ src/PositiveNegativeZero.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class PositiveNegativeZero {
+import java.util.Scanner;
+
+public class PositiveNegativeZero {
+    public static void main(String s[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        checkNumber(number);
+    }
+    private static void checkNumber(int num){
+        if (num > 0) {
+            System.out.println("postive");
+        } else if (num < 0) {
+            System.out.println("Negative");
+        } else if (num == 0) {
+            System.out.println("Zero");
+        }
+
+    }
 }
